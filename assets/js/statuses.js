39e0d7e5d4be80d471ba9263b3e618db6dae28b2
MinsungMus <isaackim42@gmail.com>
2026-08-27 00:09:00 -0400

    Create statuses.js

---
+++ assets/js/statuses.js
@@ -0,0 +1,15 @@
+const statuses = [
+    "🐶 Eating",
+    "💤 Sleeping",
+    "🧸 Playing with a toy",
+    "👀 Staring at you",
+    "🚶 Waiting for a walk",
+    "🍖 Thinking about treats"
+];
+
+function peanutStatus() {
+    const randomIndex = Math.floor(Math.random() * statuses.length);
+
+    document.getElementById("status").textContent =
+        statuses[randomIndex];
+}
