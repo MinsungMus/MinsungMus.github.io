a78c31089f439a019bc4abd6bf02f115f524c58c
MinsungMus <isaackim42@gmail.com>
2026-08-30 19:42:28 -0400

    Update pats.js

--- assets/js/pats.js
+++ assets/js/pats.js
@@ -1,7 +1,9 @@
+// Keep track of how many times Peanut has been patted.
 let pats = 0;
 
+// Increase Peanut's pat count and update the counter displayed on the page.
 function addPeanut() {
     pats++;
 
-    document.getElementById("counter").textContent = pats;
+    document.getElementById("counter").textContent = pats; // Finds html element and changes the element's displayed number.
 }
